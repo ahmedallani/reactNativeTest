b8eb3151d50b682f753faaa9ef8df61e1598363d
Ahmedghaarbii <ahmeedghaarbii@gmail.com>
2021-06-15 11:08:04 +0100

    init

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+  },
+});
+
+export default App;
