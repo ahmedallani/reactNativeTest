61f0712eb4b77a6fc54127b4d83686888448c2a7
Nassir01 <you@example.com>
2021-06-16 20:36:55 +0200

    merge

--- App.js
+++ App.js
@@ -1,34 +1,15 @@
 import React from "react";
-<<<<<<< HEAD
-import { StyleSheet, Text, View, Button ,SafeAreaView} from "react-native";
-import Profile from './components/profile.js'
-import tailwind from 'tailwind-rn';
-import Demo from './components/Demo.js'
-const App = () => {
-  return (
-<Demo/>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-   
-  },
-});
-=======
 import { StyleSheet, Text, View, Button } from "react-native";
 import Splash from './components/Splash'
 
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Splash />
     </View>
   );
 };
 
->>>>>>> d5b2c9102bcfa0eb129e528452ec2438957fab1e
 
 export default App;
