35ed23d37402c83370058076d43f5f2833d6ad41
Nassir01 <you@example.com>
2021-06-17 18:10:09 +0200

    change view

--- App.js
+++ App.js
@@ -1,33 +1,55 @@
-import React from "react";
-import { StyleSheet, Text, View, Button,KeyboardAvoidingView ,TouchableWithoutFeedback,Keyboard} from "react-native";
-import Splash from './components/Splash.js'
-import Demo from './components/Demo.js'
-import SignIn from './components/SignIn.js'
-import SignUp from './components/SignUp.js'
-
-
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import Splash from "./components/Splash.js";
+import Demo from "./components/Demo.js";
+import SignIn from "./components/SignIn.js";
+import SignUp from "./components/SignUp.js";
 
 const App = () => {
+  const [view, setview] = useState("");
+  const changeView = (view) => {
+    setview(view);
+  };
+  const renderView = () => {
+    if (view === "") {
+      return <Splash changeView={(view) => changeView(view)} />;
+    }
+    if (view === "profile") {
+      return <Demo changeView={(view) => changeView(view)} />;
+    }
+    if (view === "signup") {
+      return <SignUp changeView={(view) => changeView(view)} />;
+    }
+    if (view === "signin") {
+      return <SignIn changeView={(view) => changeView(view)} />;
+    }
+  };
   return (
     <KeyboardAvoidingView
-    behavior={Platform.OS === "ios" ? "padding" : "height"}
-    style={styles.container}
-  >
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    <View >
-      <SignIn />
-    </View>
-    </TouchableWithoutFeedback></KeyboardAvoidingView>
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View>{renderView()}</View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "space-around"
+    // justifyContent: "space-around"
     // backgroundColor: '#4c69a5',
-  
     // alignItems: 'center',
     // justifyContent: 'center',
   },
-})
+});
 
 export default App;
--- components/Demo.js
+++ components/Demo.js
@@ -8,6 +8,7 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   ScrollView,
+  StyleSheet, Dimensions,StatusBar 
 } from "react-native";
 import styles, { IMAGE_HEIGHT, IMAGE_HEIGHT_SMALL } from "./styles";
 import avataaars from "../assets/avataaars.png";
@@ -26,7 +27,8 @@ class Demo extends Component {
       Date_of_Birth: "",
       Phone_Number: "",
     };
-    this.handleChange = this.handleChange.bind(this);
+   
+
   }
 
   componentWillMount() {
@@ -58,11 +60,7 @@ class Demo extends Component {
       toValue: IMAGE_HEIGHT,
     }).start();
   };
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e,
-    });
-  };
+
   render() {
     return (
       <KeyboardAvoidingView
@@ -143,5 +141,68 @@ class Demo extends Component {
     );
   }
 }
-
+// const window = Dimensions.get('window');
+//  const IMAGE_HEIGHT = window.width / 2;
+//  const IMAGE_HEIGHT_SMALL = window.width /7;
+// const styles = StyleSheet.create({
+//     container: {
+//       // backgroundColor: '#4c69a5',
+//     //   flex: 1,
+//     marginTop:60 ,
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//     textInput: {
+//       width: window.width - 30,
+//       height: 40,
+//       marginLeft:80,
+//       borderColor: "#34494E",
+//       // borderColor: "#000000",
+//       borderBottomWidth: 1,
+//     //   marginBottom: -300,
+//     },  
+//     btnContainer: {
+//       backgroundColor: "white",
+//       marginTop: 12,
+//     },  
+//     input: {
+//       height: 50,
+//       backgroundColor: '#fff',
+//       marginHorizontal: 10,
+//       marginVertical: 5,
+//       height: 40,
+//       margin: 12,
+//       borderWidth: 1,
+//      paddingVertical: 5,
+//       paddingHorizontal: 15,
+//       width: window.width - 30,
+//     },
+//     logo: {
+//       height: IMAGE_HEIGHT,
+//       resizeMode: 'contain',
+//       marginBottom: 20,
+//       padding:10,
+//       marginTop:20
+//     },
+//     inner: {
+//       padding: 24,
+//     //   flex: 1,
+//       justifyContent: "space-around",
+//     },
+//     cont: {
+//     //   flex: 1,
+//       paddingTop: StatusBar.currentHeight,
+//     },
+//     scrollView: {
+//       backgroundColor: 'white',
+//       marginHorizontal: 20,
+//     },
+//     register:{
+//       marginBottom:20, 
+//       width:window.width -100,
+//       alignItems:'center',
+//       justifyContent:'center',
+//       height:50,
+//       backgroundColor: '#ffae',}
+//   }); 
 export default Demo;
